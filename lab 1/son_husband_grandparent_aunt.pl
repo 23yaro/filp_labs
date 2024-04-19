66bd73dd43ca3ab1046cfba348b8379368e77e4c
Alexander <147336344+23yaro@users.noreply.github.com>
2024-04-19 16:08:44 +0300

    fix: изменил комментарии
    Поправил описание унифицированных и не унифицированных переменных

--- lab 1/son_husband_grandparent_aunt.pl
+++ lab 1/son_husband_grandparent_aunt.pl
@@ -58,28 +58,28 @@ women():- woman(X), print(X), nl, fail.
 children(X):- parent(X,Y), print(Y), nl, fail.
 
 % Предикат определяет является X матерью Y
-% mother(+X,-Y)
+% mother(?X,?Y)
 mother(X,Y):- woman(X), parent(X,Y).
 % Предикат выводит маму X
 % mother(+X)
 mother(X):- mother(Y,X), print(Y), nl, fail.
 
 % Предикат определяет является X братом Y
-% brother(+X,-Y)
+% brother(?X,?Y)
 brother(X,Y):- man(X),X\=Y, parent(Z,X), parent(C,Y), Z=C.
 % Предикат выводит всех братьев X
 % brothers(+X)
 brothers(X):- mother(Y,X), parent(Y,Z), man(Z), Z\=X, print(Z), nl, false.
 
 % Предикат определяет является X сыном Y
-% son(+X,-Y)
+% son(?X,?Y)
 son(X,Y):- man(X), parent(Y,X).
 % Предикат выводит всех сыновей родителя X
 % son(+X)
 son(X):- son(Y,X), man(Y), print(Y), nl, false.
 
 % Предикат определяет является X мужем Y
-% husband(+X,-Y)
+% husband(?X,?Y)
 husband(X,Y):- man(X), woman(Y), parent(X,Z), parent(Y,C), Z=C.
 % Предикат выводит мужа женщины X
 % husband(+X)
@@ -92,10 +92,10 @@ grand_pas(X) :- parent(Z, X),parent(Y, Z),man(Y),print(Y), nl, fail.
 
 
 % Предикат определяет является X тетей Y
-% aunt(+X, -Y)
+% aunt(?X, ?Y)
 aunt(X, Y) :- parent(Z, Y),sister(X, Z).
 % Предикат определяет является X сестрой Y
-% sister(+X, -Y)
+% sister(?X, ?Y)
 sister(X, Y) :- parent(Z, X),parent(Z, Y),woman(X),X \= Y.
 
 % Предикат выводит всех теть X
@@ -103,23 +103,23 @@ sister(X, Y) :- parent(Z, X),parent(Z, Y),woman(X),X \= Y.
 aunts(X) :- parent(Z, X), sister(Y, Z), print(Y), nl, fail.
 
 % Предикат определяет является X дедушкой Y
-% grand_pa(+X, -Y)
+% grand_pa(?X, ?Y)
 grand_pa(X, Y) :- man(X), parent(X, Z), parent(Z, Y).
 
 
 % Предикат, который проверяет, являются ли X и Y дедушкой и внучкой или внучкой и дедушкой
-% grand_pa_and_da(+X, -Y) или grand_pa_and_da(-X, +Y)
+% grand_pa_and_da(?X, ?Y)
 grand_pa_and_da(X, Y) :-
     (grand_pa(X, Y), woman(Y));
     (grand_pa(Y, X), woman(X)).
 
 
 % Вспомогательные предикаты
-% sibling(+X, +Y)
+% sibling(?X, ?Y)
 % Определяет братьев и сестер
 sibling(X, Y) :- parent(Z, X), parent(Z, Y), X \= Y.
 
-% sister(+X, -Y)
+% sister(?X, ?Y)
 % Определяет сестер
 sister(X, Y) :- woman(X), sibling(X, Y).
 
@@ -127,17 +127,17 @@ sister(X, Y) :- woman(X), sibling(X, Y).
 % Предикат выводит всех дедушек X
 grand_pas(X) :- parent(Z, X), parent(Y, Z), man(Y), print(Y), nl, fail.
 
-% grand_pa(+X, -Y)
+% grand_pa(?X, ?Y)
 % Предикат определяющий дедушку
 grand_pa(X, Y) :- man(X), parent(X, Z), parent(Z, Y).
 
-% grand_pa_and_da(+X, -Y) или grand_pa_and_da(-X, +Y)
+% grand_pa_and_da(?X, ?Y) или grand_pa_and_da(-X, +Y)
 % Итоговый предикат проверяет отношения дедушки и внучки или внучки и дедушки
 grand_pa_and_da(X, Y) :-
     (grand_pa(X, Y), woman(Y));
     (grand_pa(Y, X), woman(X)).
 
-% aunt(+X, -Y)
+% aunt(?X, ?Y)
 % Предикат проверяет является ли X тетей Y
 aunt(X, Y) :- parent(Z, Y), sister(X, Z).
 
