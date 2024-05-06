b1e75094fc77945fdf4c310af4a41fe19c7c2cdf
Alexander <147336344+23yaro@users.noreply.github.com>
2024-05-06 16:39:57 +0300

    Lab 5.8 задача с ресурса projecteuler

--- lab_5/clear_functions/src/ProjectEuler.kt
+++ lab_5/clear_functions/src/ProjectEuler.kt
@@ -1,2 +1,21 @@
-class ProjectEuler {
+fun main() {
+    var totalSum = 0
+
+    for (num in 2..999999) {
+        if (num == getSumOfDigits(num, 0)) {
+            totalSum += num
+        }
+    }
+
+    println("Сумма всех чисел, которые можно записать как сумму пятых степеней их цифр: $totalSum")
+}
+
+fun getSumOfDigits(number: Int, sumSoFar: Int): Int {
+    if (number == 0) {
+        return sumSoFar
+    } else {
+        val digit = number % 10
+        val newSum = sumSoFar + Math.pow(digit.toDouble(), 5.0).toInt()
+        return getSumOfDigits(number / 10, newSum)
+    }
 }
